1bd2d1d79d437ca23b870646a85a8f10c1f7f38d
BayleyPap <bayley.w.papworth@gmail.com>
2026-03-26 22:29:18 +1100

    feat(github-client): add profile and repo fetching with rate limit and 404 handling

---
+++ phase-1/02-github-client/github_client.py
@@ -0,0 +1,55 @@
+import requests
+
+USERNAME = "BayleyPap"
+PROFILEURL = f"https://api.github.com/users/{USERNAME}"
+REPOURL = f"https://api.github.com/users/{USERNAME}/repos?sort=pushed&per_page=10"
+
+
+def fetch_api(endpoint: str):
+    try:
+        response = requests.get(
+            endpoint,
+            timeout=5,
+        )
+        remaining = int(response.headers.get("X-RateLimit-Remaining", 1))
+        if remaining == 0:
+            return {"error": "Rate limit reached. Try again later."}
+        if response.status_code == 404:
+            return {"error": "User not found"}
+        response.raise_for_status()
+        data = response.json()
+
+        return data
+
+    except requests.exceptions.ConnectionError:
+        return {"error": "Connection failed"}
+    except requests.exceptions.HTTPError as e:  # Need to handle 404 and 403 seperately
+        return {"error": f"HTTP error: {e}"}
+    except requests.exceptions.Timeout:
+        return {"error": "Connection timeout"}
+
+
+def get_profile():
+    return fetch_api(PROFILEURL)
+
+
+def get_repos():
+    repos = fetch_api(REPOURL)
+    return sorted(repos, key=lambda r: r["pushed_at"], reverse=True)
+
+
+def print_summary():
+    profile = get_profile()
+    repos = get_repos()
+    print(f"""
+====={profile["name"]}({profile["login"]})=====
+{profile["bio"]}
+Followers: {profile["followers"]}
+Public repo count: {profile["public_repos"]}
+Last pushs:""")
+    for repo in repos[:3]:
+        print(f"{repo['name']} - {repo['pushed_at']}")
+
+
+if __name__ == "__main__":
+    print_summary()
